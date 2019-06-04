0f2e55cffb162522eec8e05aaaa23f29f186e905
Cher <red_phoenix417@yahoo.com>
2019-06-04 01:12:58 +0000

    parsed the body result to to pass as an object and loop through the keys to iterate/console log the avatar_url only

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,31 @@
 var request = require('request');
+let tokenKey = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
+    let options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': 'token ' + tokenKey.GITHUB_TOKEN
+        }
+      };
+
+    request(options, function(err, res, body) {
+
+    let parsedBody = JSON.parse(body)
+    
+    cb(err, parsedBody);
+    }); 
   }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+
+  getRepoContributors("jquery", "jquery", function(err, parsedBody)
+{
+    parsedBody.forEach(value => {
+        console.log(value.avatar_url);
+    })
+    // console.log("Errors:", err);
+    // console.log("Result:", result);
   });
