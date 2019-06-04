b716114314a363de3b446fb2f3fd9b4ce7076499
Cher <red_phoenix417@yahoo.com>
2019-06-04 03:58:06 +0000

    making comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,61 +1,60 @@
-var args = process.argv;
+let args = process.argv
 
-var owner = process.argv[2];
-var repo = process.argv[3];
+let owner = process.argv[2]
+let repo = process.argv[3]
 
 if (process.argv.length < 4) {
-    console.log("Provide an owner and repo name");
-    return ;
+  console.log('Provide an owner and repo name')
+  return;
 }
-// console.log(owner, repo);
 
+// import the request library
+let request = require('request')
+// import my token from a file I ignored
+let tokenKey = require('./secrets.js')
 
-var request = require('request');
-let tokenKey = require('./secrets.js');
+console.log('Welcome to the GitHub Avatar Downloader!')
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
-function getRepoContributors(repoOwner, repoName, cb) {
-    let options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-          'User-Agent': 'request',
-          'Authorization': 'token ' + tokenKey.GITHUB_TOKEN
-        }
-      };
-
-    //   *********************************************
-
-    request(options, function(err, res, body) {
-
+// make API called to Github to retrieve the cotributors from the specified repository
+function getRepoContributors (repoOwner, repoName, cb) {
+    // get properties from the "body", and adding User-Agent and Athorization to the header
+  let options = {
+    url:
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      Authorization: `token ${tokenKey.GITHUB_TOKEN}`
+    }
+  }
+  // where the function (API query) happens
+  request(options, function (err, res, body) {
     let parsedBody = JSON.parse(body)
-    
-    cb(err, parsedBody);
-    }); 
+    // the callback function from the parent function
+    cb(err, parsedBody)
+  })
 }
 
-    // **********************************************
+// **********************************************
+// Getting the URL and saving it to the filPath
 
-let fs = require('fs');
+let fs = require('fs')
 
-function downloadImageByURL(url, filePath) {
-    request
-        .get(url)
-        .pipe(fs.createWriteStream(filePath))
+function downloadImageByURL (url, filePath) {
+  request
+    .get(url)
+    .pipe(fs.createWriteStream(filePath))
 }
-  
+
 // *******************************************
 
-getRepoContributors(owner, repo, function(err, parsedBody) {
+// calls the functions
+getRepoContributors(owner, repo, function (err, parsedBody) {
+  parsedBody.forEach(value => {
+    let url = value.avatar_url
+    let login = value.login
+    let filePath = `avatars/${login}.jpg`
 
-    parsedBody.forEach(value => {
-        let url = value.avatar_url;
-        let login = value.login;
-        let filePath = "avatars/" + login + ".jpg";
+    downloadImageByURL(url, filePath)
+  })
 
-        downloadImageByURL(url, filePath) 
-
-    })
-    // console.log("Errors:", err);
-    // console.log("Result:", result);
-});
+})
